ec952a9871ebf2d6a5d5f35fb6db68d6b7da46d1
PARVEZ <parvez.s@canishub.com>
2020-04-17 13:14:23 +0530

    changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const app = express();
-app.use('/',express.static(__dirname+'/static'))
+// const express = require('express');
+// const app = express();
+// app.use('/',express.static(__dirname+'/static'))
 
-app.listen(process.env.PORT);
+// app.listen(4400);
